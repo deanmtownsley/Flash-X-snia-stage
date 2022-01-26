4e68531870887d26b2b319376df110f1b38b2697
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2022-01-26 17:08:02 -0500

    switching back to singularity backend for incompFlow/LidDrivenCavity test

--- tests/incompFlow/LidDrivenCavity.py
+++ tests/incompFlow/LidDrivenCavity.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     # name: name of the image
     # base: remote image of flashx environment
     # backend: docker/singularity
-    image = maple.Image(name='incomp_flow',base='akashdhruv/flashtest:latest',backend='docker')
+    image = maple.Image(name='incomp_flow',base='docker://akashdhruv/flashtest:latest',backend='singularity')
     
     # create a container object
     # name: name of the local container
