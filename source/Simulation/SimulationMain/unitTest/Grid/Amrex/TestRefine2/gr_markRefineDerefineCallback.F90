4782d52fd0ad0b66d1cc943514c20b50fb1e9424
Klaus Weide <>
2022-01-31 18:12:46 -0600

    Copyright and license notices in .F90 files...
    Make existing notices more like what the LICENSE file recommends,
    but also make it fit better into the Robodoc header style.

--- source/Simulation/SimulationMain/unitTest/Grid/Amrex/TestRefine2/gr_markRefineDerefineCallback.F90
+++ source/Simulation/SimulationMain/unitTest/Grid/Amrex/TestRefine2/gr_markRefineDerefineCallback.F90
@@ -1,12 +1,15 @@
 subroutine gr_markRefineDerefineCallback(lev, tags, time, tagval, clearval) bind(c)
+!! NOTICE
+!!  Copyright 2022 UChicago Argonne, LLC and contributors
+!!
 !!  Licensed under the Apache License, Version 2.0 (the "License");
 !!  you may not use this file except in compliance with the License.
-!! 
-!! Unless required by applicable law or agreed to in writing, software
-!! distributed under the License is distributed on an "AS IS" BASIS,
-!! WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-!! See the License for the specific language governing permissions and
-!! limitations under the License.
+!!
+!!  Unless required by applicable law or agreed to in writing, software
+!!  distributed under the License is distributed on an "AS IS" BASIS,
+!!  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+!!  See the License for the specific language governing permissions and
+!!  limitations under the License.
    use iso_c_binding
    use amrex_fort_module,      ONLY : wp => amrex_real
    use amrex_box_module,       ONLY : amrex_box
