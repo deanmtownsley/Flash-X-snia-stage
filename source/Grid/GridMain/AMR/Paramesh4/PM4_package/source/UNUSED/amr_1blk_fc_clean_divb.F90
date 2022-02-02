c0dfda77d03f2e5b24e8e1f5501f30d3005acbd7
Klaus Weide <>
2022-02-01 18:24:05 -0600

    Some more license tweaks, comment cleanup
    Some fixes / edits in copyright / license notices in indicidual files.
    
    Also remove gratuitous 1-line differences from the FLASH5
    version of many files under PM4_package.

--- source/Grid/GridMain/AMR/Paramesh4/PM4_package/source/UNUSED/amr_1blk_fc_clean_divb.F90
+++ source/Grid/GridMain/AMR/Paramesh4/PM4_package/source/UNUSED/amr_1blk_fc_clean_divb.F90
@@ -1,15 +1,11 @@
-
-!! NOTICE
-!!  Copyright 2022 UChicago Argonne, LLC and contributors
-!!
-!!  Licensed under the Apache License, Version 2.0 (the "License");
-!!  you may not use this file except in compliance with the License.
-!!
-!!  Unless required by applicable law or agreed to in writing, software
-!!  distributed under the License is distributed on an "AS IS" BASIS,
-!!  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-!!  See the License for the specific language governing permissions and
-!!  limitations under the License.
+!----------------------------------------------------------------------
+! PARAMESH - an adaptive mesh library.
+! Copyright (C) 2003
+!
+! Use of the PARAMESH software is governed by the terms of the
+! usage agreement which can be found in the file
+! 'PARAMESH_USERS_AGREEMENT' in the main paramesh directory.
+!----------------------------------------------------------------------
 
 !     Michael L. Rilee, December 2002, *clean_divb*
 !        Support for projecting field onto divergenceless field
