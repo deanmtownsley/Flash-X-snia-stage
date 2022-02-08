a169449166de3fa9c9f5e491cd15cc7f04a28532
Thomas Klosterman <tklos96@gmail.com>
2022-02-08 15:25:43 -0600

    fixing issues with amr_sort_tools

--- source/Grid/GridMain/AMR/amr_sort_tools.F90
+++ source/Grid/GridMain/AMR/amr_sort_tools.F90
@@ -28,7 +28,7 @@
 !! ARGUMENTS
 !!
 !!  integer,intent(in) :: nprocs, mype, totblocks
-!!  real,intent(in) :: work(maxblocks_tr)
+!!  real,intent(in) :: work(10*MAXBLOCKS)
 !!  integer,intent(inout) :: mort_cutoffs(nprocs)
 !!
 !!***
@@ -86,7 +86,7 @@
 !! ARGUMENTS
 !!
 !!  integer,intent(in) :: nprocs, mype, lnblockst
-!!  real,intent(in) :: worktemp(maxblocks_tr)
+!!  real,intent(in) :: worktemp(10*MAXBLOCKS)
 !!  integer,intent(inout) :: mort_cutoffs(nprocs)
 !!
 !!***
@@ -95,8 +95,7 @@
                                           lnblockst,mort_cutoffs)
 
 !-----Use statements
-      use tree, only : maxblocks_tr
-      use paramesh_comm_data, ONLY : amr_mpi_meshComm, amr_mpi_real
+      use Grid_data, ONLY : gr_meshComm
 
       implicit none
 
@@ -105,14 +104,14 @@
 
 !-----Input/output statements
       integer,intent(in)    :: nprocs, mype, lnblockst
-      real,intent(in)       :: worktemp(maxblocks_tr)
+      real,intent(in)       :: worktemp(10*MAXBLOCKS)
       integer,intent(inout) :: mort_cutoffs(nprocs)
 
 !-----Local variables and arrays
       integer :: j,ierr
       integer :: totblocks
       integer :: lnblockst_arr(0:nprocs-1)
-      real    :: worktempbuf(maxblocks_tr)
+      real    :: worktempbuf(10*MAXBLOCKS)
       real    :: locwork, totwork
       real    :: work_so_far, work_per_proc
       integer :: current_proc, current_block
@@ -121,14 +120,14 @@
 !-----Gather list of lnblockst for each proc
       call MPI_ALLGATHER(lnblockst,1,MPI_INTEGER,  &
                          lnblockst_arr,1,MPI_INTEGER,    &
-                         amr_mpi_meshComm,ierr)
+                         gr_meshComm,ierr)
 
       totblocks = sum(lnblockst_arr)
 
 !-----Sum total work across procs
       locwork = sum(worktemp(1:lnblockst))
-      call MPI_ALLREDUCE(locwork,totwork,1,amr_mpi_real, &
-                         MPI_SUM,amr_mpi_meshComm,ierr)
+      call MPI_ALLREDUCE(locwork,totwork,1,MPI_REAL, &
+                         MPI_SUM,gr_meshComm,ierr)
 
 
 !-----Initialize to ensure mort_cutoffs(i) <= mort_cutoffs(i+1)
@@ -141,8 +140,8 @@
       do j=0,nprocs-1
 !-------Proc-by-proc broadcast work list from worktempbuf
         if (j.eq.mype) worktempbuf = worktemp
-        call MPI_BCAST(worktempbuf,lnblockst_arr(j),amr_mpi_real,  &
-                       j,amr_mpi_meshComm,ierr)
+        call MPI_BCAST(worktempbuf,lnblockst_arr(j),MPI_REAL,  &
+                       j,gr_meshComm,ierr)
 
 !-------Apply algorithm to update mort_cutoffs
         call sort_algorithm(nprocs,work_per_proc,&
@@ -177,7 +176,7 @@
 !!  real,intent(inout)    :: work_so_far
 !!  integer,intent(inout) :: current_proc,current_block
 !!  integer, intent(in)   :: totblocks,nblocks
-!!  real,intent(in)       :: workbuf(maxblocks_tr)
+!!  real,intent(in)       :: workbuf(totblocks)
 !!  integer,intent(out)   :: mort_cutoffs(nprocs)
 !!
 !!***
