c90837b522dd19cf6d33cb8ed067de831fa875bf
Jared O Neal <joneal@anl.gov>
2022-09-29 10:59:50 -0500

    (Issue #215) First draft of Milhoja Orchestration implementation.
    No testing of unit so far.

---
+++ source/Orchestration/OrchestrationMain/Milhoja/Orchestration_executeTasks_Cpu.F90
@@ -0,0 +1,40 @@
+!! @copyright Copyright 2022 UChicago Argonne, LLC and contributors
+!!
+!! @licenseblock
+!! Licensed under the Apache License, Version 2.0 (the "License");
+!! you may not use this file except in compliance with the License.
+!!
+!! Unless required by applicable law or agreed to in writing, software
+!! distributed under the License is distributed on an "AS IS" BASIS,
+!! WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+!! See the License for the specific language governing permissions and
+!! limitations under the License.
+!! @endlicenseblock
+!!
+!! @file
+
+!> @ingroup OrchestrationMilhoja
+!! @stubref{Orchestration_executeTasks_Cpu}
+!!
+!! @brief Concrete implementation of Orchestration_executeTasks_Cpu
+subroutine Orchestration_executeTasks_Cpu(MH_taskFunction, nThreads)
+    use milhoja_types_mod,   ONLY : MILHOJA_INT
+    use milhoja_runtime_mod, ONLY : milhoja_runtime_taskFunction, &
+                                    milhoja_runtime_executeTasks_Cpu
+
+    use Orchestration_interface, ONLY : Orchestration_checkInternalError
+
+    implicit none
+
+    procedure(milhoja_runtime_taskFunction)            :: MH_taskFunction
+    integer,                                intent(IN) :: nThreads
+
+    integer(MILHOJA_INT) :: MH_nThreads
+    integer(MILHOJA_INT) :: MH_ierr
+
+    MH_nThreads = INT(nThreads, kind=MILHOJA_INT)
+
+    CALL milhoja_runtime_executeTasks_Cpu(MH_taskFunction, MH_nThreads, MH_ierr)
+    CALL Orchestration_checkInternalError("Orchestration_executeTasks_Cpu", MH_ierr)
+end subroutine Orchestration_executeTasks_Cpu
+
