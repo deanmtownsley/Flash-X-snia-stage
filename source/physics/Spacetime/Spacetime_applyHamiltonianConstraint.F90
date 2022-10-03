0a8613f6b12e5e09a2919d751094111ff9c44a52
Steven Fromm <fromm@nscl.msu.edu>
2022-10-03 17:12:07 -0400

    (+) Stress-energy and constraint procedures

---
+++ source/physics/Spacetime/Spacetime_applyHamiltonianConstraint.F90
@@ -0,0 +1,34 @@
+!> @copyright Copyright 2022 UChicago Argonne, LLC and contributors
+!!
+!! @licenseblock
+!!   Licensed under the Apache License, Version 2.0 (the "License");
+!!   you may not use this file except in compliance with the License.
+!!
+!!   Unless required by applicable law or agreed to in writing, software
+!!   distributed under the License is distributed on an "AS IS" BASIS,
+!!   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+!!   See the License for the specific language governing permissions and
+!!   limitations under the License.
+!! @endlicenseblock
+!!
+!! @file
+!! @brief Spacetime_applyHamiltonianConstraint stub
+
+!> @ingroup Spacetime
+!!
+!! @brief Apply the Hamiltonian constraint
+!!
+!! @details
+!! @anchor Spacetime_applyHamiltonianConstraint_stub
+!!
+!! This procedure is responsible for applying the Hamiltonian constraint
+!! if required by an implementation.
+!!
+!! @param  t  Time of the current solution of the evolved variables
+subroutine Spacetime_applyHamiltonianConstraint(t)
+   implicit none
+
+   real, intent(in) :: t
+
+   return
+end subroutine Spacetime_applyHamiltonianConstraint
