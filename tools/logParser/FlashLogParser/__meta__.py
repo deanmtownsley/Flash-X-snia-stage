ed9bada811c863195381365ada6b09e21938c380
akashdhruv <akashdhruv@gwmail.gwu.edu>
2022-10-04 12:09:21 -0500

    add log parser tool

---
+++ tools/logParser/FlashLogParser/__meta__.py
@@ -0,0 +1,8 @@
+"""Metadata for FlashLogParser"""
+
+__pkgname__     = 'FlashLogParser'
+__version__     = '0.0.0'
+__authors__     = 'The Flash-X Team'
+__license__     = 'Apache License'
+__copyright__   = 'Copyright (c) The Flash-X Team 2022. All Rights Reserved.'
+__description__ = 'Flash-X Log Parser'
---
+++ tools/logParser/README.md
@@ -0,0 +1,9 @@
+
+## FlashLogParser
+
+Parser for Flash-X log files to collect performance data and statistics
+
+Installation
+```
+python3 setup.py develop --user
+```
