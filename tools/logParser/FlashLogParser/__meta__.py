743e149f49ba3b7aac190f8113c9dd261f055e82
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2022-10-04 15:44:27 -0500

    remove logParser

--- tools/logParser/FlashLogParser/__meta__.py
+++
@@ -1,8 +0,0 @@
-"""Metadata for FlashLogParser"""
-
-__pkgname__     = 'FlashLogParser'
-__version__     = '0.0.0'
-__authors__     = 'The Flash-X Team'
-__license__     = 'Apache License'
-__copyright__   = 'Copyright (c) The Flash-X Team 2022. All Rights Reserved.'
-__description__ = 'Flash-X Log Parser'
