9d852f5a77607d7d9840465525bb71590a966145
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2022-11-14 21:52:18 -0600

    update split

--- source/Simulation/testsplit.py
+++ source/Simulation/testsplit.py
@@ -39,33 +39,48 @@ def createSuiteDict(infoNode, suiteDict):
 
 if __name__ == "__main__":
 
-    # Parse test.info
+    # Parse test.info from the working directory
+    # using xml parser from FlashTest backend
     infoNode = FlashXTest.backend.FlashTest.lib.xmlNode.parseXml("test.info")
 
-    # Get suiteDict
+    # Create a dictionary for test nodes
+    # using the recursive function createSuiteDict
     suiteDict = {}
     createSuiteDict(infoNode, suiteDict)
 
+    # Create a list of setupNames using information
+    # from suiteDict
     setupList = []
     for key in suiteDict.keys():
         setupList.append(suiteDict[key]["setupName"])
 
+    # Remove duplicates from the list
     setupList = [*set(setupList)]
+    print(f"set of setupList: {setupList}")
 
+    # Create list of keys from existing test.info and
+    # designated where they should belong
+    # in "tests.yaml" or test suite
     yamlkeys = ["setupOptions", "parfiles", "restartParfiles", "transfers"]
     suitekeys = ["numProcs"]
 
-    with open("gce.suite", "w") as suitefile:
+    # Open *.suite file and start populating test suite
+    with open("test.suite", "w") as suitefile:
 
         suitefile.write("# Test suite file for test gce\n")
         suitefile.write('# comments start with "#"\n')
 
+        # Loop over setupList
         for setupName in setupList:
 
             suitefile.write(f"\n# {setupName}\n")
 
+            # Open a tests.yaml file in setupName/tests folder and
+            # start adding tests
             with open(f"SimulationMain/{setupName}/tests/tests.yaml", "w") as yamlfile:
 
+                # Get list of test nodes matching
+                # the setup name
                 nodeList = [
                     node
                     for node in suiteDict.keys()
@@ -75,8 +90,13 @@ if __name__ == "__main__":
                 yamlfile.write("# YAML file for test information\n")
                 yamlfile.write('# comments start with "#"\n')
 
+                # Loop over nodeList and start populating
                 for nodeName in nodeList:
 
+                    # Write information to yaml file
+                    # This logic is a bit hacky to
+                    # to manage paths for parfiles and restartParfiles
+                    # TODO: ?? How will paths for transfers be handled ??
                     yamlfile.write(f"\n{nodeName}:\n")
                     for key in yamlkeys:
                         if key in suiteDict[nodeName].keys():
@@ -97,6 +117,7 @@ if __name__ == "__main__":
 
                             yamlfile.write(f"  {key}: {suiteDict[nodeName][key]}\n")
 
+                    # Write line to suitefile
                     suitefile.write(
                         f'{setupName} --test="{nodeName}" --nprocs={suiteDict[nodeName]["numProcs"]}\n'
                     )
