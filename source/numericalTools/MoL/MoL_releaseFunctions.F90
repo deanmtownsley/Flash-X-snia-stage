312841c1e45a99cc2d8cfa41f1ef2fc19dedae2b
Steven Fromm <fromm@nscl.msu.edu>
2023-03-01 09:41:35 -0500

    (*) Copyright 2022 -> Copyright 2023

--- source/numericalTools/MoL/MoL_releaseFunctions.F90
+++ source/numericalTools/MoL/MoL_releaseFunctions.F90
@@ -1,6 +1,6 @@
 !> @file
 !!
-!! @copyright Copyright 2022 UChicago Argonne, LLC and contributors
+!! @copyright Copyright 2023 UChicago Argonne, LLC and contributors
 !!
 !! @licenseblock
 !!   Licensed under the Apache License, Version 2.0 (the "License");
