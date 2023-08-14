50d2b282755dddf55c816884affccd160dea97d3
Youngjun Lee <youngjunlee7@gmail.com>
2023-08-14 14:26:55 -0500

    style fixes

--- source/TimeAdvance/TimeAdvanceMain/StrangSplit/TimeAdvance.F90
+++ source/TimeAdvance/TimeAdvanceMain/StrangSplit/TimeAdvance.F90
@@ -31,68 +31,65 @@
 #include "Simulation.h"
 #include "constants.h"
 
-subroutine TimeAdvance(dt, dtold, time )
-  
-  use Hydro_interface,     ONLY : Hydro,Hydro_gravPotIsAlreadyUpdated
-  use Gravity_interface,   ONLY : Gravity_potential
-  use RadTrans_interface,  ONLY : RadTrans
-  use Particles_interface, ONLY : Particles_advance, Particles_dump
-  use Burn_interface, ONLY : Burn
-  use Deleptonize_interface, ONLY: Deleptonize
-  use Timers_interface, ONLY : Timers_start, Timers_stop
-  implicit none
-  
-  real, intent(IN) :: dt, dtold, time
-  
+subroutine TimeAdvance(dt, dtold, time)
 
-     call Hydro(time, dt, dtOld)
+   use Hydro_interface, ONLY: Hydro, Hydro_gravPotIsAlreadyUpdated
+   use Gravity_interface, ONLY: Gravity_potential
+   use RadTrans_interface, ONLY: RadTrans
+   use Particles_interface, ONLY: Particles_advance, Particles_dump
+   use Burn_interface, ONLY: Burn
+   use Deleptonize_interface, ONLY: Deleptonize
+   use Timers_interface, ONLY: Timers_start, Timers_stop
+   implicit none
+
+   real, intent(IN) :: dt, dtold, time
+
+   call Hydro(time, dt, dtOld)
 #ifdef DEBUG_ADVANCE
-     print*,'returned from hydro '
+   print *, 'returned from hydro '
 #endif
 
 #ifndef DRIVER_DIFFULAST
-     ! 3. Diffusive processes:
-     call RadTrans(dt)
+   ! 3. Diffusive processes:
+   call RadTrans(dt)
 #ifdef DEBUG_ADVANCE
-     print*,'returned from RadTrans'
+   print *, 'returned from RadTrans'
 #endif
 #endif
 
-     ! 4. Add source terms:
-     call Timers_start("sourceTerms")
-     call Burn(dt)
-     call Deleptonize(.false., dt, time)
+   ! 4. Add source terms:
+   call Timers_start("sourceTerms")
+   call Burn(dt)
+   call Deleptonize(.false., dt, time)
 
-     call Timers_stop("sourceTerms")
+   call Timers_stop("sourceTerms")
 #ifdef DEBUG_ADVANCE
-     print*,'returned from sourceTerms'
+   print *, 'returned from sourceTerms'
 #endif
 
 #ifdef DRIVER_DIFFULAST
-     ! 3. Diffusive processes: *** CHANGED ORDER !!! ***
-     !    Radiation, viscosity, conduction, & magnetic registivity
-     call RadTrans(dt)
+   ! 3. Diffusive processes: *** CHANGED ORDER !!! ***
+   !    Radiation, viscosity, conduction, & magnetic registivity
+   call RadTrans(dt)
 #endif
 
-
-        ! #. Advance Particles
-        call Timers_start("Particles_advance")
-        call Particles_advance(dtOld, dt)
-        call Timers_stop("Particles_advance")
+   ! #. Advance Particles
+   call Timers_start("Particles_advance")
+   call Particles_advance(dtOld, dt)
+   call Timers_stop("Particles_advance")
 #ifdef DEBUG_ADVANCE
-        print*, 'return from Particles_advance '  ! DEBUG
+   print *, 'return from Particles_advance '  ! DEBUG
 #endif
 
-     !Allows evolution of gravitational potential for Spark Hydro
-     ! #. Calculate gravitational potentials
-     if (.NOT. Hydro_gravPotIsAlreadyUpdated()) then
-        call Timers_start("Gravity potential")
-        call Gravity_potential()
-        call Timers_stop("Gravity potential")
+   !Allows evolution of gravitational potential for Spark Hydro
+   ! #. Calculate gravitational potentials
+   if (.NOT. Hydro_gravPotIsAlreadyUpdated()) then
+      call Timers_start("Gravity potential")
+      call Gravity_potential()
+      call Timers_stop("Gravity potential")
 #ifdef DEBUG_ADVANCE
-     print*, 'return from Gravity_potential '  ! DEBUG
+      print *, 'return from Gravity_potential '  ! DEBUG
 #endif
-     endif
-
+   end if
 
 end subroutine TimeAdvance
