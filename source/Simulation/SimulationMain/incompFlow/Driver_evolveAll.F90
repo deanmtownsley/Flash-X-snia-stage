118718c57d0c1d9c7cc07441893c923f1ab9d05f
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2023-09-14 13:10:03 -0500

    add flux correction routines

--- source/Simulation/SimulationMain/incompFlow/Driver_evolveAll.F90
+++ source/Simulation/SimulationMain/incompFlow/Driver_evolveAll.F90
@@ -58,7 +58,9 @@ subroutine Driver_evolveAll()
 
    use Grid_interface, ONLY: Grid_updateRefinement, Grid_setInterpValsGcell, &
                              Grid_fillGuardCells, Grid_getTileIterator, &
-                             Grid_releaseTileIterator, Grid_solvePoisson, Grid_solveLaplacian
+                             Grid_releaseTileIterator, Grid_solvePoisson, Grid_solveLaplacian, &
+                             Grid_correctFluxData, Grid_putFluxData, Grid_communicateFluxes, &
+                             Grid_getMaxRefinement
 
    use Grid_iterator, ONLY: Grid_iterator_t
 
@@ -70,7 +72,7 @@ subroutine Driver_evolveAll()
                                  IncompNS_setupPoisson, IncompNS_corrector, &
                                  IncompNS_indicators, IncompNS_reInitGridVars, &
                                  IncompNS_advection, IncompNS_diffusion, IncompNS_getScalarProp, &
-                                 IncompNS_getGridVar
+                                 IncompNS_getGridVar, IncompNS_fluxSet, IncompNS_fluxUpdate
 
    use Multiphase_interface, ONLY: Multiphase_setFluidProps, Multiphase_setPressureJumps, &
                                    Multiphase_advection, Multiphase_redistance, Multiphase_solve, &
@@ -370,6 +372,56 @@ subroutine Driver_evolveAll()
       call Grid_fillGuardCells(CENTER_FACES, ALLDIR, &
                                maskSize=NUNK_VARS + NDIM*NFACE_VARS, mask=gcMask)
 
+         ! Enforce flux correction for velocities
+         ! currently exclusively implmented for
+         ! AMReX configuration
+#ifdef FLASH_GRID_AMREX
+         !----------------------------------------------------------------
+         call Grid_getMaxRefinement(maxLev, mode=1)
+
+         !----------------------------------------------------------------
+         do level = maxLev, 1, -1
+
+            !----------------------------------------------------------------
+            if (level < maxLev) then
+               call Grid_communicateFluxes(ALLDIR, level)
+            end if
+            !----------------------------------------------------------------
+
+            !----------------------------------------------------------------
+            call Grid_getTileIterator(itor, LEAF, level=level, tiling=.FALSE.)
+            do while (itor%isValid())
+               !----------------------------------------------------------------
+               call itor%currentTile(tileDesc)
+               call tileDesc%getDataPtr(fluxBufX, FLUXX)
+               call tileDesc%getDataPtr(fluxBufY, FLUXY)
+               call tileDesc%getDataPtr(fluxBufZ, FLUXZ)
+               !----------------------------------------------------------------
+               fluxBufX = 0.
+               fluxBufY = 0.
+               fluxBufZ = 0.
+               call IncompNS_fluxSet(tileDesc, fluxBufX, fluxBufY, fluxBufZ, tileDesc%limits(LOW, :))
+
+               if (level < maxLev) then
+                  call Grid_correctFluxData(tileDesc, fluxBufX, fluxBufY, fluxBufZ, tileDesc%limits(LOW, :))
+               end if
+
+               call IncompNS_fluxUpdate(tileDesc, fluxBufX, fluxBufY, fluxBufZ, tileDesc%limits(LOW, :))
+
+               if (level > 1) then
+                  call Grid_putFluxData(tileDesc, fluxBufX, fluxBufY, fluxBufZ, tileDesc%limits(LOW, :))
+               end if
+
+               !----------------------------------------------------------------
+               call tileDesc%releaseDataPtr(fluxBufX, FLUXX)
+               call tileDesc%releaseDataPtr(fluxBufY, FLUXY)
+               call tileDesc%releaseDataPtr(fluxBufZ, FLUXZ)
+               call itor%next()
+            end do
+            call Grid_releaseTileIterator(itor)
+         end do 
+#endif
+
       ! Start of fractional-step velocity procedure
       ! Calculate predicted velocity and apply
       ! necessary forcing
@@ -399,6 +451,56 @@ subroutine Driver_evolveAll()
       call Grid_fillGuardCells(CENTER_FACES, ALLDIR, &
                                maskSize=NUNK_VARS + NDIM*NFACE_VARS, mask=gcMask)
       ins_predcorrflg = .false.
+
+         ! Enforce flux correction for velocities
+         ! currently exclusively implmented for
+         ! AMReX configuration
+#ifdef FLASH_GRID_AMREX
+         !----------------------------------------------------------------
+         call Grid_getMaxRefinement(maxLev, mode=1)
+
+         !----------------------------------------------------------------
+         do level = maxLev, 1, -1
+
+            !----------------------------------------------------------------
+            if (level < maxLev) then
+               call Grid_communicateFluxes(ALLDIR, level)
+            end if
+            !----------------------------------------------------------------
+
+            !----------------------------------------------------------------
+            call Grid_getTileIterator(itor, LEAF, level=level, tiling=.FALSE.)
+            do while (itor%isValid())
+               !----------------------------------------------------------------
+               call itor%currentTile(tileDesc)
+               call tileDesc%getDataPtr(fluxBufX, FLUXX)
+               call tileDesc%getDataPtr(fluxBufY, FLUXY)
+               call tileDesc%getDataPtr(fluxBufZ, FLUXZ)
+               !----------------------------------------------------------------
+               fluxBufX = 0.
+               fluxBufY = 0.
+               fluxBufZ = 0.
+               call IncompNS_fluxSet(tileDesc, fluxBufX, fluxBufY, fluxBufZ, tileDesc%limits(LOW, :))
+
+               if (level < maxLev) then
+                  call Grid_correctFluxData(tileDesc, fluxBufX, fluxBufY, fluxBufZ, tileDesc%limits(LOW, :))
+               end if
+
+               call IncompNS_fluxUpdate(tileDesc, fluxBufX, fluxBufY, fluxBufZ, tileDesc%limits(LOW, :))
+
+               if (level > 1) then
+                  call Grid_putFluxData(tileDesc, fluxBufX, fluxBufY, fluxBufZ, tileDesc%limits(LOW, :))
+               end if
+
+               !----------------------------------------------------------------
+               call tileDesc%releaseDataPtr(fluxBufX, FLUXX)
+               call tileDesc%releaseDataPtr(fluxBufY, FLUXY)
+               call tileDesc%releaseDataPtr(fluxBufZ, FLUXZ)
+               call itor%next()
+            end do
+            call Grid_releaseTileIterator(itor)
+         end do 
+#endif
 
       ! Calculate divergence of predicted velocity
       !------------------------------------------------------------
