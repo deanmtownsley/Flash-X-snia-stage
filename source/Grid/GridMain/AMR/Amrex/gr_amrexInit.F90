b2dea21bb43e458c58c3013a6f8d44ecb9ce7e4c
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2023-09-21 13:59:06 -0500

    update amrex bittree messages

--- source/Grid/GridMain/AMR/Amrex/gr_amrexInit.F90
+++ source/Grid/GridMain/AMR/Amrex/gr_amrexInit.F90
@@ -150,7 +150,7 @@ subroutine gr_amrexInit()
   call pp_amr%add   ("use_bittree", gr_amrexUseBittree)
 #else
   if(gr_meshMe==MASTER_PE) then
-     write(*,*) "AMReX is not configured with Bittree. This feature will not be available and may lead to performance degradation"
+     write(*,*) "The AMReX library was not configured with Bittree. The gr_amrexUseBittree feature will not be available to Flash-X. This may result in performance degradation"
   end if
 #endif
 
