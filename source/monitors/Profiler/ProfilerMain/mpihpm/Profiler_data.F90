5d540e66c0d0dc4fbedeb012b67a1cc29f9a7c26
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2023-10-24 19:28:06 -0500

    [Profiler] Add a generic name for evolution

--- source/monitors/Profiler/ProfilerMain/mpihpm/Profiler_data.F90
+++ source/monitors/Profiler/ProfilerMain/mpihpm/Profiler_data.F90
@@ -26,6 +26,6 @@
 
 module Profiler_data
   implicit none
-  character (len=*), parameter :: prf_evolutionName = 'Flashx_evolution'
+  character (len=*), parameter :: prf_evolutionName = 'evolution'
   logical, save :: prf_evolutionOnly
 end module Profiler_data
