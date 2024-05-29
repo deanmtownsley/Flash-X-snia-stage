39cc5e188b883a145a26d3011d91fbad1e3f8e20
adubey64 <adubey@anl.gov>
2024-05-29 15:28:03 -0500

    continue with weaklib

--- source/physics/Eos/EosMain/WeakLib/eos_weaklib.F90
+++ source/physics/Eos/EosMain/WeakLib/eos_weaklib.F90
@@ -28,7 +28,7 @@
 !!
 !!
 !!***
-SUBROUTINE eos_weaklib(mode,vecLen,eosData,xPres, xTemp, xDens, xGamc,xEntr,xAbar,xZbar,massFrac,mask)
+SUBROUTINE eos_weaklib(mode,vecLen,eosData,xPres, xTemp, xDens, xGamc, xEner, xEntr,xAbar,xZbar,massFrac,mask)
 
   USE Driver_interface, ONLY : Driver_abort
   USE eos_weaklib_inter, ONLY: eos_weaklib_short
@@ -43,14 +43,14 @@ SUBROUTINE eos_weaklib(mode,vecLen,eosData,xPres, xTemp, xDens, xGamc,xEntr,xAba
   INTEGER, INTENT(in) :: mode, vecLen
   ! EOS_NUM defined in Eos.h
   REAL, INTENT(inout), DIMENSION(vecLen*EOS_NUM) :: eosData
-  REAL, INTENT(inout), DIMENSION(vecLen) :: xDens,xTemp,xPres, xGamc,xEntr,xAbar,xZbar
+  REAL, INTENT(inout), DIMENSION(vecLen) :: xDens,xTemp,xPres, xGamc,xEntr,xAbar,xZbar,xEner
   REAL, OPTIONAL,INTENT(in), DIMENSION(vecLen*NSPECIES) :: massFrac
   ! must correspond to dimensions of Eos_wrapped
   LOGICAL,OPTIONAL, DIMENSION(EOS_VARS+1:EOS_NUM),INTENT(in)::mask
 
   INTEGER :: pres, temp, dens, gamc, eint, game, abar, zbar, entr, &
              elef
-  REAL, DIMENSION(vecLen) :: xYe, xCs2, xA, xZ,xEner
+  REAL, DIMENSION(vecLen) :: xYe, xCs2, xA, xZ
   INTEGER :: xMode, err
 
   err = 0
@@ -82,8 +82,8 @@ SUBROUTINE eos_weaklib(mode,vecLen,eosData,xPres, xTemp, xDens, xGamc,xEntr,xAba
 
 
   xYe   = eosData(elef+1:elef+vecLen)
-  xEner = eosData(eint+1:eint+vecLen)
-  xEntr = eosData(entr+1:entr+vecLen)
+!!$  xEner = eosData(eint+1:eint+vecLen)
+!!$  xEntr = eosData(entr+1:entr+vecLen)
 
 !!$  PRINT*, 'eos_weaklib  before interpolation  Z', &
 !!$          MAXVAL(eosData(zbar+1:zbar+vecLen) ), &
@@ -112,7 +112,7 @@ SUBROUTINE eos_weaklib(mode,vecLen,eosData,xPres, xTemp, xDens, xGamc,xEntr,xAba
 !!$    CALL Driver_abort("[EOS] problem with weaklib EOS")
 !!$  END IF
  
-      eosData(eint+1:eint+vecLen) = xEner
+!!$      eosData(eint+1:eint+vecLen) = xEner
 !!$      eosData(entr+1:entr+vecLen) = xEntr
       eosData(elef+1:elef+vecLen) = xYe 
 
