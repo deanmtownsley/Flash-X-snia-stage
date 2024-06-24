7aedfbdeacd45ed5999b1068857be210d29fa4e7
adubey64 <adubey@anl.gov>
2024-06-24 14:17:02 -0500

    doxygenified the Gamma law Eos

--- source/physics/Eos/EosMain/Gamma/eos_idealGammaInit.F90
+++ source/physics/Eos/EosMain/Gamma/eos_idealGammaInit.F90
@@ -1,33 +1,23 @@
-!!****if* source/physics/Eos/EosMain/Gamma/eos_idealGammaInit
-!! NOTICE
-!!  Copyright 2022 UChicago Argonne, LLC and contributors
+!> @copyright Copyright 2023 UChicago Argonne, LLC and contributors
 !!
-!!  Licensed under the Apache License, Version 2.0 (the "License");
-!!  you may not use this file except in compliance with the License.
+!! @licenseblock
+!!   Licensed under the Apache License, Version 2.0 (the "License");
+!!   you may not use this file except in compliance with the License.
 !!
-!!  Unless required by applicable law or agreed to in writing, software
-!!  distributed under the License is distributed on an "AS IS" BASIS,
-!!  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-!!  See the License for the specific language governing permissions and
-!!  limitations under the License.
+!!   Unless required by applicable law or agreed to in writing, software
+!!   distributed under the License is distributed on an "AS IS" BASIS,
+!!   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+!!   See the License for the specific language governing permissions and
+!!   limitations under the License.
+!! @endlicenseblock
 !!
-!! NAME
+!! @file
+!> @ingroup physics_Eos
 !!
-!!  eos_idealGammaInit
-!!
-!! SYNOPSIS
-!!  
-!!  call  eos_idealGammaInit()
-!!                 
-!!
-!! DESCRIPTION
-!!
-!!  This routine does ideal gamma law specific initialization
-!!
-!!  ARGUMENTS
+!! @brief This routine does ideal gamma law specific initialization
 !!
 !!
-!!***
+!!
 
 #include "Eos.h"
 #include "constants.h"
