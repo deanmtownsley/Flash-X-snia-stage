ff251c89900fc8f673e11f6018108682e54b2d73
Josh Martin <joshua.martin.1@stonybrook.edu>
2025-06-18 15:20:09 -0400

    Removed much of the extraneous commented code which was primarily past code from Eos_wrapped.F90

--- source/physics/Eos/EosMain/Helmholtz/ExternalAbarZbar/Eos_multiDim.F90
+++ source/physics/Eos/EosMain/Helmholtz/ExternalAbarZbar/Eos_multiDim.F90
@@ -12,7 +12,7 @@
 !!
 !! DESCRIPTION
 !!
-!! Josh Martin 2025
+!! Josh Martin 2025 - !DQ How should I change this stuff?
 !!
 !! This function is provided for the user's convenience and acts as a simple
 !! wrapper to the Eos interface. The Eos interface uses a single, flexible data
@@ -95,7 +95,6 @@ subroutine Eos_multiDim(mode,range,solnData)
    use Eos_data, ONLY: eos_eintSwitch, eos_smalle, eos_meshMe
    use Driver_interface, ONLY : Driver_abort
    use Logfile_interface, ONLY: Logfile_stampMessage 
-   !JM use Eos_interface, ONLY : Eos
    use Eos_interface, ONLY : Eos_vector
    use eos_localInterface, ONLY : eos_externalComputeAbarZbar
  
@@ -107,28 +106,18 @@ subroutine Eos_multiDim(mode,range,solnData)
  
    integer, intent(in) :: mode
    integer, dimension(2,MDIM), intent(in) :: range
-  !JMJN integer,intent(in) :: blockID
  
    real, pointer:: solnData(:,:,:,:)
  
- !JM #ifndef FIXEDBLOCKSIZE
    real, allocatable, dimension(:):: energyKinetic,energyInternal
-   !JM real, allocatable :: eosData(:),massFraction(:)
-   real, allocatable :: eosData(:,:) !JM
+   real, allocatable :: eosData(:,:)
    integer, allocatable,dimension(:) :: iFlag
- !JM #else
- !JM  real, dimension(MAXCELLS):: energyKinetic,energyInternal
-   !JM real, dimension(NSPECIES*MAXCELLS) :: massFraction
-   !JM real, dimension(EOS_NUM*MAXCELLS) :: eosData
- !JM   real, dimension(MAXCELLS, EOS_NUM) :: eosData
- !JM   integer, dimension(MAXCELLS) :: iFlag
- !JM #endif
  
  
    integer :: ierr, dataStruct
    integer :: i,j,k, vecLen,pres,dens,gamc,temp,abar,zbar,eint,entr
  
-   integer :: index, index_start, index_end !JMJN
+   integer :: index, index_start, index_end
  
  !! ---------------------------------------------------------------------------------
    ! Test calling arguments
@@ -152,48 +141,18 @@ subroutine Eos_multiDim(mode,range,solnData)
  
    ! Initializations:   grab the solution data from UNK and determine
    !   the length of the data being operated upon
-
-   !JM vecLen = range(HIGH,IAXIS)-range(LOW,IAXIS)+1 (I don't know why this would only be 1D)
  
-    !JM I think VecLen is the number of rows in eosData
+   !VecLen is the number of rows in eosData
     vecLen = (range(HIGH,IAXIS)-range(LOW,IAXIS)+1)*& !JM
              (range(HIGH,JAXIS)-range(LOW,JAXIS)+1)*& !JM
              (range(HIGH,KAXIS)-range(LOW,KAXIS)+1) !JM
  
-    if (vecLen==0) return !JM * Return immediately for empty IAXIS range! (for efficiency and avoiding index range errors)
- 
-   
-   ! These integers are indexes into the location in eosData just before the storage area for the appropriate variable.
-   !JM pres = (EOS_PRES-1) !JM *vecLen
-   !JM dens = (EOS_DENS-1) !JM *vecLen
-   !JM temp = (EOS_TEMP-1) !JM *vecLen
-   !JM gamc = (EOS_GAMC-1) !JM *vecLen
-   !JM eint = (EOS_EINT-1) !JM *vecLen
-   !JM abar = (EOS_ABAR-1) !JM *vecLen
-   !JM zbar = (EOS_ZBAR-1) !JM *vecLen
-   !JM entr = (EOS_ENTR-1) !JM *vecLen
- 
- !#ifndef FIXEDBLOCKSIZE
+    if (vecLen==0) return ! Return immediately for empty IAXIS range! (for efficiency and avoiding index range errors)
+
  allocate(energyInternal(vecLen))
  allocate(energyKinetic(vecLen))
- !JM allocate(massFraction(NSPECIES*vecLen))
- !JM allocate(eosData(EOS_NUM*vecLen))
  allocate(eosData(vecLen, EOS_NUM)) !DQ What is the difference between EOS_NUM and EOS_VARS?
- allocate(iFlag(vecLen)) !JM I still need to change this?
- !#endif  
- 
-   !JM do k = range(LOW,KAXIS), range(HIGH,KAXIS)
-      !JM do j = range(LOW,JAXIS), range(HIGH,JAXIS)
- 
-         !! Fill up two scratch arrays. 
-         !! energyKinetic holds velocity vector information -- 1/2 * Vmag**2
-         !! energyInternal holds eint (directly)  or energyTotal - ekinetic (calculated),
-         !!          depending upon eintSwitch
- 
-         !JM energyKinetic(1:vecLen) = 0.5*(solnData(VELX_VAR,range(LOW,IAXIS):range(HIGH,IAXIS),j,k)**2 + &
-     !JM &                                  solnData(VELY_VAR,range(LOW,IAXIS):range(HIGH,IAXIS),j,k)**2 + &
-     !JM &                                  solnData(VELZ_VAR,range(LOW,IAXIS):range(HIGH,IAXIS),j,k)**2)
- 
+ allocate(iFlag(vecLen))  
  
  !JM My version of energyKinetic (per gram) and energyInternal (per gram)
  
@@ -210,22 +169,14 @@ subroutine Eos_multiDim(mode,range,solnData)
           &                              solnData(VELY_VAR,i,j,k)**2 + &
           &                              solnData(VELZ_VAR,i,j,k)**2)
  #ifdef EINT_VAR
-             !JM initial values of EINT that may be overwritten
-             !DQ Does having to access and then reaccess the array like this slow the program down?
-             !JM energyInternal(index) = solnData(EINT_VAR,i,j,k)
-             !JM Instead of doing potential overwriting, I am just doing a classic if/then/else
- 
-             !JM Here comes the potential overwriting. EInt should be
-             !JM ETotal - Ekinetic, but this can only be calculated
-             !JM to good precision if ETotal and EKinetic are different
-             !JM enough. (Have Dean check this understanding). !DQ
+             !DQ Question on overwriting instead of if/else
              if (solnData(ENER_VAR,i,j,k) > &
                 (1.+ eos_eintSwitch)*energyKinetic(index)) then
                 energyInternal(index) = solnData(ENER_VAR,i,j,k) - energyKinetic(index)
              else 
                 energyInternal(index) = solnData(EINT_VAR,i,j,k)
              end if
-             !JM bringing Eint up to an Efloor if necessary
+             ! Bringing Eint up to an Efloor if necessary
              energyInternal(index) = max(energyInternal(index), eos_smalle)
  #else
              !JM So now if we don't have the EINT_VAR stored, we just
@@ -244,7 +195,7 @@ subroutine Eos_multiDim(mode,range,solnData)
  
           if (index > vecLen) then !DEBUG
              call Driver_abort("Index exceeds vecLen in Eos_multiDim!") !DEBUG
-          end if !DEBUG
+          end if !DEBUG !DQ should I leave this here?
           
           index = index + 1
  
@@ -263,14 +214,14 @@ subroutine Eos_multiDim(mode,range,solnData)
     end do
  end do
  
- !JM Now that eosData has obtained all necessary data from grid, we
- !JM call Eos_vector
+ ! Now that eosData has obtained all necessary data from grid, we
+ ! call Eos_vector
  
  call Eos_vector(mode,vecLen,eosData)
  
- !JM Now that Eos_vector has been called, we need to write
- !JM the completed Eos variable calculations (eosData) back
- !JM to the grid (which is solnData).
+ ! Now that Eos_vector has been called, we need to write
+ ! the completed Eos variable calculations (eosData) back
+ ! to the grid (which is solnData).
  
  index = 1
  iFlag = 0 !DQ is this like iFlag = np.zeros(vecLen)?
@@ -297,7 +248,7 @@ subroutine Eos_multiDim(mode,range,solnData)
              iFlag(index) = 1
           end if
           
-          if (index > vecLen) then !DEBUG
+          if (index > vecLen) then !DEBUG !DQ
              call Driver_abort("Index exceeds vecLen in Eos_multiDim!") !DEBUG
           end if !DEBUG
  
@@ -331,7 +282,7 @@ subroutine Eos_multiDim(mode,range,solnData)
           solnData(GAME_VAR,i,j,k) = eosData(index, EOS_PRES)/&
           (eosData(index, EOS_EINT) *eosData(index, EOS_DENS)) + 1.0
  
-          if (index > vecLen) then !DEBUG
+          if (index > vecLen) then !DEBUG !DQ
              call Driver_abort("Index exceeds vecLen in Eos_multiDim!") !DEBUG
           end if !DEBUG
  
@@ -340,98 +291,10 @@ subroutine Eos_multiDim(mode,range,solnData)
        end do
     end do
  end do
-                
- 
- 
- !JM #ifdef EINT_VAR
- !JM          energyInternal(1:vecLen) = solnData(EINT_VAR,range(LOW,IAXIS):range(HIGH,IAXIS),j,k)
- !JM 
- !JM          do i = 1,vecLen
- !JM             if (solnData(ENER_VAR,range(LOW,IAXIS)+i-1,j,k) > &
- !JM                (1.+ eos_eintSwitch)*energyKinetic(i)) then
- !JM                energyInternal(i) = solnData(ENER_VAR,range(LOW,IAXIS)+i-1,j,k) - energyKinetic(i)
- !JM             end if
- !JM             energyInternal(i) = max(energyInternal(i), eos_smalle)
- !JM             massFraction((i-1)*NSPECIES+1:i*NSPECIES) = &
- !JM                solnData(SPECIES_BEGIN:SPECIES_END,range(LOW,IAXIS)+i-1,j,k)
- !JM          end do
- !JM #else
- !JM         do i = 1,vecLen
- !JM            energyInternal(i) = solnData(ENER_VAR,range(LOW,IAXIS)+i-1,j,k) - energyKinetic(i)
- !JM            energyInternal(i) = max(energyInternal(i), eos_smalle)
- !JM            massFraction((i-1)*NSPECIES+1:i*NSPECIES) = &
- !JM                 solnData(SPECIES_BEGIN:SPECIES_END,range(LOW,IAXIS)+i-1,j,k)
- !JM         end do
- !JM #endif
- 
- !JM         eosData(pres+1:pres+vecLen) = &
- !JM              solnData(PRES_VAR,range(LOW,IAXIS):range(HIGH,IAXIS),j,k)
- !JM         eosData(dens+1:dens+vecLen) = &
- !JM              solnData(DENS_VAR,range(LOW,IAXIS):range(HIGH,IAXIS),j,k)
- !JM         eosData(temp+1:temp+vecLen) = &
- !JM              solnData(TEMP_VAR,range(LOW,IAXIS):range(HIGH,IAXIS),j,k)
- !JM         eosData(gamc+1:gamc+vecLen) = &
- !JM              solnData(GAMC_VAR,range(LOW,IAXIS):range(HIGH,IAXIS),j,k)
- !JM         eosData(eint+1:eint+vecLen) = energyInternal(1:vecLen)
- 
-         ! Call out for external computation of abar and zbar based on
-         ! other scalars.  This calls the callback for whoever
-         ! requested the ExternalAbarZbar implementation.
- !JM         call eos_externalComputeAbarZbar(solnData(SPECIES_BEGIN:UNK_VARS_END,&
- !JM              range(LOW,IAXIS):range(HIGH,IAXIS),j,k), &
- !JM              eosData(abar+1:abar+vecLen), eosData(zbar+1:zbar+vecLen) )
- 
- !JM         call Eos(mode,vecLen,eosData,massFraction)
-         
- !JM         solnData(PRES_VAR,range(LOW,IAXIS):range(HIGH,IAXIS),j,k) = &
- !JM              eosData(pres+1:pres+vecLen)
- !JM         solnData(TEMP_VAR,range(LOW,IAXIS):range(HIGH,IAXIS),j,k) = &
- !JM              eosData(temp+1:temp+vecLen)
- !JM         solnData(GAMC_VAR,range(LOW,IAXIS):range(HIGH,IAXIS),j,k) = &
- !JM              eosData(gamc+1:gamc+vecLen)
- !JM #ifdef EINT_VAR
- !JM         solnData(EINT_VAR,range(LOW,IAXIS):range(HIGH,IAXIS),j,k) = &
- !JM              eosData(eint+1:eint+veclen)
- !JM #endif
- !JM         solnData(ENER_VAR,range(LOW,IAXIS):range(HIGH,IAXIS),j,k) = &
- !JM              eosData(eint+1:eint+veclen) + energyKinetic(1:vecLen)
- !JM #ifdef ENTR_VAR
- !JM         solnData(ENTR_VAR,range(LOW,IAXIS):range(HIGH,IAXIS),j,k) = &
- !JM              eosData(entr+1:entr+veclen)
- !JM #endif
- 
-         ! check for zero values before calculating gamma
- !JM         iFlag = 0
- !JM         where ( (eosData(eint+1:eint+vecLen) .eq. 0.) .or. (eosData(dens+1:dens+vecLen) .eq. 0.))
- !JM            iFlag(1:vecLen) = 1
- !JM         end where
- 
-         !maybe there was a wrong flag set
- !JM         if (maxval(iFlag) .gt. 0) then
- !JM            if (eos_meshMe .EQ. MASTER_PE) then
- !JM               write(*,*) "ERROR After calling Eos, eosData(EOS_EINT) or eosData(EOS_DENS) are zero"
- !JM               write(*,*) "  Perhaps the initialization routine is wrong..... or"
- !JM               write(*,*) "  perhaps the runtime parameter eosMode is wrong."
- !JM               write(*,*) "  This routine Eos_wrapped was called with mode= ", mode
- !JM               write(*,*) "     Check constants.h to determine value of MODE_DENS_??"
- !JM            endif
- !JM            call Logfile_stampMessage('[Eos_wrapped] ERROR Density or Internal Energy are zero after a call to EOS!')
- !JM            call Driver_abort('[Eos_wrapped] ERROR Density or Internal Energy are zero after a call to EOS!')
- !JM         end if
- 
- !JM         solnData(GAME_VAR,range(LOW,IAXIS):range(HIGH,IAXIS),j,k) = &
- !JM              eosData(pres+1:pres+veclen)/&
- !JM              (eosData(eint+1:eint+veclen) *eosData(dens+1:dens+veclen)) + 1.0
- 
- !JM      end do
- !JM   end do
- 
- !JM #ifndef FIXEDBLOCKSIZE
-   deallocate(energyKinetic)
-   deallocate(energyInternal)
-   deallocate(eosData)
-   deallocate(iFlag)
- !JM  deallocate(massFraction)
- !JM #endif
- !JM  return
- end subroutine Eos_multiDim
+
+deallocate(energyKinetic)
+deallocate(energyInternal)
+deallocate(eosData)
+deallocate(iFlag)
+
+end subroutine Eos_multiDim
