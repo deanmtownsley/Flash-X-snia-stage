087958621ed77b34083d150119d0658fc7d85de2
Josh Martin <joshua.martin.1@stonybrook.edu>
2026-07-02 15:01:12 -0400

    Added stub for Driver_initMaterialProperties.F90
    I think I finally understand why we include stubs in Fortran. Stubs simplify the main code. To give an example of this, this commit puts a stub in the Driver folder with the actual subroutine in Driver/DriverMain. Now, we are always going to probably use DriverMain, but let's say we chose not to by making the necessary change in the Driver config file. Instead of having to check if we are using DriverMain to see if we want to call the actual subroutine, the subroutine is called no matter what, but the config file would tell it to load the stub from the Driver folder rather than the actual subroutine. At thees a les unecessary feature checks and thus simplifies the code.

---
+++ source/Driver/Driver_initMaterialProperties.F90
@@ -0,0 +1,27 @@
+!!****f* source/Driver/Driver_initMaterialProperties
+!!
+!! NAME
+!!  Driver_initMaterialProperties
+!!
+!! SYNOPSIS
+!!
+!!   call Driver_initMaterialProperties()
+!!
+!! DESCRIPTION
+!!
+!!   Initializes material properties units by
+!!   calling their respective initialization routines 
+!!    
+!! ARGUMENTS
+!!
+!!   
+!!
+!!***
+
+subroutine Driver_initMaterialProperties()
+
+  implicit none
+  return
+
+
+end subroutine Driver_initMaterialProperties
